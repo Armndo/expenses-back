1a6b1fc335a1193309da8a1273cc8542dcf7b6ef
Armando Gonzalez <armndo.g@gmail.com>
2025-02-20 16:27:09 -0600

    feat: created sources controller with basic authenticated index route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\SourceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post("login", [UserController::class, "login"]);
 Route::get("info", [UserController::class, "info"])->middleware("auth:api");
+Route::get("sources", [SourceController::class, "index"])->middleware("auth:api");
