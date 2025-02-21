f8957aad293ca1c5c0a9578bcb6c07e84a8bf332
Armando Gonzalez <armndo.g@gmail.com>
2025-02-20 23:10:20 -0600

    feat(expensecontroller): implemented date range for index internally

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -12,11 +12,17 @@ class ExpenseController extends Controller
 {
     public function index(Request $request) {
         $user = Auth::user();
-        $end = $request->date;
-        $start = new Carbon($end);
-        $start->day = 1;
-        $start = $start->format("Y-m-d");
-        $expenses = $user->sources()->with(["expenses" => fn(Builder $q) => $q->whereBetween("date", [$start, $end])->orderBy("date")])->get()->toArray();
+        $date = Carbon::now();
+        $date->day = 1;
+        $start = $date->format("Y-m-d");
+        $date->month += 1;
+        $date->day -= 1;
+        $end = $date->format("Y-m-d");
+        $expenses = $user->sources()->with(["expenses" => fn(Builder $query) =>
+            $query->whereBetween("date", [$start, $end])
+            ->whereNull("instalments")
+            ->orderBy("date")
+        ])->get()->toArray();
 
         return $expenses;
     }
