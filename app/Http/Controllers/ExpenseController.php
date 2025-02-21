0d607a487bb13de22fe840e3aea58ceb57b38e28
Armando Gonzalez <armndo.g@gmail.com>
2025-02-21 11:19:29 -0600

    feat(expensecontroller): implemented store, update and destroy expenses functions and endpoints

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Expense;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,5 +25,67 @@ class ExpenseController extends Controller
         ])->get()->toArray();
 
         return $expenses;
+    }
+
+    public function store(Request $request) {
+        $user = Auth::user();
+        $source = $user->sources()->where("id", $request->source_id)->first();
+
+        if (!$source) {
+            return response()->json("error", 400);
+        }
+
+        try {
+            $expense = $source->expenses()->create($request->only("date", "amount", "description"));
+        } catch (Exception $e) {
+            return response()->json("error", 400);
+        }
+
+        return $expense;
+    }
+
+    public function update(Request $request, $expense_id) {
+        $user = Auth::user();
+        $source = $user->sources()->where("id", $request->source_id)->first();
+
+        if (!$source) {
+            return response()->json("error", 400);
+        }
+
+        $expense = $source->expenses()->where("id", $expense_id)->first();
+
+        if (!$expense) {
+            return response()->json("error", 400);
+        }
+
+        try {
+            $expense->fill($request->only("date", "amount", "description"));
+            $expense->save();
+        } catch (Exception $e) {
+            return response()->json("error", 400);
+        }
+
+        return $expense;
+    }
+
+    public function destroy(Request $request, $expense_id) {
+        $user = Auth::user();
+        $source = $user->sources()->where("id", $request->source_id)->first();
+
+        if (!$source) {
+            return response()->json("error", 400);
+        }
+
+        $expense = $source->expenses()->where("id", $expense_id)->first();
+
+        if (!$expense) {
+            return response()->json("error", 400);
+        }
+
+        try {
+            $expense->delete();
+        } catch (Exception $e) {
+            return response()->json("error", 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::post("login", [UserController::class, "login"]);
 Route::get("info", [UserController::class, "info"])->middleware("auth:api");
 Route::get("expenses", [ExpenseController::class, "index"])->middleware("auth:api");
+Route::post("expenses", [ExpenseController::class, "store"])->middleware("auth:api");
+Route::put("expenses/{expense_id}", [ExpenseController::class, "update"])->middleware("auth:api");
+Route::delete("expenses/{expense_id}", [ExpenseController::class, "destroy"])->middleware("auth:api");
